2c0e291a006a4cde27fd9b7853de9d9d7f2bcb56
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-20 11:54:27 +1100

    🙃

--- src/main/java/io/github/cadiboo/examplemod/server/ServerEventSubscriber.java
+++ src/main/java/io/github/cadiboo/examplemod/server/ServerEventSubscriber.java
@@ -3,12 +3,12 @@ package io.github.cadiboo.examplemod.server;
 import io.github.cadiboo.examplemod.util.ModReference;
 import net.minecraftforge.fml.common.Mod;
 
-import static net.minecraftforge.fml.relauncher.Side.CLIENT;
+import static net.minecraftforge.fml.relauncher.Side.SERVER;
 
 /**
  * Subscribe to events that should be handled on the PHYSICAL/DEDICATED SERVER in this class
  */
-@Mod.EventBusSubscriber(modid = ModReference.MOD_ID, value = CLIENT)
+@Mod.EventBusSubscriber(modid = ModReference.MOD_ID, value = SERVER)
 public final class ServerEventSubscriber {
 
 }
