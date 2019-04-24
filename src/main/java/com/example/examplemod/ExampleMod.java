87f3551ab9efd90028942b59bc2350d3ecf332a9
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-04-24 15:41:56 +1000

    comment out the example mod

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+//@Mod("examplemod")
 public class ExampleMod
 {
     // Directly reference a log4j logger.
