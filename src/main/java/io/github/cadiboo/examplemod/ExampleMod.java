8adebd68855259c4f7ee493dd685915df46b8eca
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-05-10 17:33:37 +1000

    Yay, needlessly logging stuff because tutorial

--- src/main/java/io/github/cadiboo/examplemod/ExampleMod.java
+++ src/main/java/io/github/cadiboo/examplemod/ExampleMod.java
@@ -1,6 +1,8 @@
 package io.github.cadiboo.examplemod;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author Cadiboo
@@ -9,5 +11,11 @@ import net.minecraftforge.fml.common.Mod;
 public final class ExampleMod {
 
 	public static final String MODID = "examplemod";
+
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+	public ExampleMod() {
+		LOGGER.debug("Hello from Example Mod!");
+	}
 
 }
