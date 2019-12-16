b284d83c4df672929d2b03aae9db0a2dda4fede2
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-12-16 16:52:44 +1100

    Fix unfinished comment

--- src/main/java/io/github/cadiboo/examplemod/config/ExampleModConfig.java
+++ src/main/java/io/github/cadiboo/examplemod/config/ExampleModConfig.java
@@ -7,7 +7,8 @@ import java.util.List;
 /**
  * This holds the baked (runtime) values for our config.
  * These values should never be from changed outside this package.
- * This can be split into
+ * This can be split into multiple classes (Server, Client, Player, Common)
+ * but has been kept in one class for simplicity
  *
  * @author Cadiboo
  */
