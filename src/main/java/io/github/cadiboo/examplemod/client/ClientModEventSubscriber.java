95bf083daf2b4acd412385a7687456ca09140909
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-12-19 19:38:18 +1100

    Update comment

--- src/main/java/io/github/cadiboo/examplemod/client/ClientModEventSubscriber.java
+++ src/main/java/io/github/cadiboo/examplemod/client/ClientModEventSubscriber.java
@@ -29,6 +29,9 @@ public final class ClientModEventSubscriber {
 	private static final Logger LOGGER = LogManager.getLogger(ExampleMod.MODID + " Client Mod Event Subscriber");
 
 	/**
+	 * We need to register our renderers on the client because rendering code does not exist on the server
+	 * and trying to use it on a dedicated server will crash the game.
+	 * <p>
 	 * This method will be called by Forge when it is time for the mod to do its client-side setup
 	 * This method will always be called after the Registry events.
 	 * This means that all Blocks, Items, TileEntityTypes, etc. will all have been registered already
