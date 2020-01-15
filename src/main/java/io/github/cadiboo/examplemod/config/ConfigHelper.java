fa6f14b51a3d99a9307294edb924569895fd89d1
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-01-16 10:31:38 +1100

    Not many people will change their config in-code

--- src/main/java/io/github/cadiboo/examplemod/config/ConfigHelper.java
+++ src/main/java/io/github/cadiboo/examplemod/config/ConfigHelper.java
@@ -10,44 +10,22 @@ import net.minecraftforge.fml.config.ModConfig;
  */
 public final class ConfigHelper {
 
-	// We store a reference to the ModConfigs here to be able to change the values in them from our code
-	// (For example from a config GUI)
-	private static ModConfig clientConfig;
-	private static ModConfig serverConfig;
-
 	public static void bakeClient(final ModConfig config) {
-		clientConfig = config;
-
 		ExampleModConfig.clientBoolean = ConfigHolder.CLIENT.clientBoolean.get();
 		ExampleModConfig.clientStringList = ConfigHolder.CLIENT.clientStringList.get();
 		ExampleModConfig.clientDyeColorEnum = ConfigHolder.CLIENT.clientDyeColorEnum.get();
 
 		ExampleModConfig.modelTranslucency = ConfigHolder.CLIENT.modelTranslucency.get();
 		ExampleModConfig.modelScale = ConfigHolder.CLIENT.modelScale.get();
-
 	}
 
 	public static void bakeServer(final ModConfig config) {
-		serverConfig = config;
-
 		ExampleModConfig.serverBoolean = ConfigHolder.SERVER.serverBoolean.get();
 		ExampleModConfig.serverStringList = ConfigHolder.SERVER.serverStringList.get();
 		ExampleModConfig.serverEnumDyeColor = ConfigHolder.SERVER.serverEnumDyeColor.get();
 
 		ExampleModConfig.electricFurnaceEnergySmeltCostPerTick = ConfigHolder.SERVER.electricFurnaceEnergySmeltCostPerTick.get();
 		ExampleModConfig.heatCollectorTransferAmountPerTick = ConfigHolder.SERVER.heatCollectorTransferAmountPerTick.get();
-	}
-
-	/**
-	 * Helper method to set a value on a config and then save the config.
-	 *
-	 * @param modConfig The ModConfig to change and save
-	 * @param path      The name/path of the config entry
-	 * @param newValue  The new value of the config entry
-	 */
-	public static void setValueAndSave(final ModConfig modConfig, final String path, final Object newValue) {
-		modConfig.getConfigData().set(path, newValue);
-		modConfig.save();
 	}
 
 }
