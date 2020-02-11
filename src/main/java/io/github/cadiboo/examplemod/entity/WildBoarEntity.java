ad6f94288a763f881c42f9ea21e2bb904252eb30
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-02-12 09:02:28 +1100

    Update WildBoarEntity.java

--- src/main/java/io/github/cadiboo/examplemod/entity/WildBoarEntity.java
+++ src/main/java/io/github/cadiboo/examplemod/entity/WildBoarEntity.java
@@ -1,16 +1,20 @@
 package io.github.cadiboo.examplemod.entity;
 
-import io.github.cadiboo.examplemod.init.ModEntityTypes;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.passive.horse.AbstractHorseEntity;
+import net.minecraft.network.IPacket;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
+import net.minecraftforge.fml.network.FMLPlayMessages;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 /**
  * A Wild Boar entity.
  * Literally just a pig (with a different texture).
- * TODO: Will have more stuff added to it soon.
+ * TODO: Will have more stuff added to it soon. (Will charge at player
  *
  * @author Cadiboo
  */
@@ -18,10 +22,6 @@ public class WildBoarEntity extends PigEntity {
 
 	public WildBoarEntity(final EntityType<? extends WildBoarEntity> entityType, final World world) {
 		super(entityType, world);
-	}
-
-	public WildBoarEntity(final World world) {
-		super(ModEntityTypes.WILD_BOAR.get(), world);
 	}
 
 	@Override
@@ -35,10 +35,34 @@ public class WildBoarEntity extends PigEntity {
 		this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(baseHealth * 1.5D);
 	}
 
+	/**
+	 * Creates a child new entity from the parent entity.
+	 * Can be used to set additional on the child entity based on the parent.
+	 *
+	 * @param parent The entity that made this child
+	 * @return A new WildBoar
+	 * @see AbstractHorseEntity#setOffspringAttributes(AgeableEntity, AbstractHorseEntity)
+	 */
 	@Override
-	public WildBoarEntity createChild(AgeableEntity parent) {
+	public WildBoarEntity createChild(final AgeableEntity parent) {
 		// Use getType to support overrides in subclasses
 		return (WildBoarEntity) getType().create(this.world);
+	}
+
+	/**
+	 * Called on the logical server to get a packet to send to the client containing data necessary to spawn your entity.
+	 * Using Forge's method instead of the default vanilla one allows extra stuff to work such as sending extra data,
+	 * using a non-default entity factory and having {@link IEntityAdditionalSpawnData} work.
+	 *
+	 * It is not actually necessary for our WildBoarEntity to use Forge's method as it doesn't need any of this extra
+	 * functionality, however, this is an example mod and many modders are unaware that Forge's method exists.
+	 *
+	 * @return The packet with data about your entity
+	 * @see FMLPlayMessages.SpawnEntity
+	 */
+	@Override
+	public IPacket<?> createSpawnPacket() {
+		return NetworkHooks.getEntitySpawningPacket(this);
 	}
 
 }
